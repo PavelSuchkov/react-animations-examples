cdb8f1813c642e9622c011eeafc5ff60d7c516e6
Pavel-Suchkou <suchkovp@gmail.com>
2023-02-27 21:20:36 +0300

    examples with Transition and CSSTransition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {Transition, CSSTransition} from "react-transition-group";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //  пример работы кастомногоспиннера + react.Transition
+    // const [loaderVisible, setLoaderVisible] = useState(false)
+    //
+    // return (
+    //     <div className="app">
+    //         <button onClick={() => {setLoaderVisible(!loaderVisible)}}>{loaderVisible ? 'Hide' : 'Show'}</button>
+    //         <div className="wrap">
+    //             <Transition
+    //                 in={loaderVisible}  // триггер активации анимации
+    //                 timeout={500}
+    //                 mountOnEnter
+    //                 unmountOnExit
+    //             >
+    //                 {state => <div className={`circle ${state}`}/>}
+    //             </Transition>
+    //         </div>
+    //     </div>
+    // );
+
+    //пример работы кастомного спиннера + react CssTransition
+    const [loaderVisible, setLoaderVisible] = useState(false)
+
+    return (
+        <div className="app">
+            <button onClick={() => {
+                setLoaderVisible(!loaderVisible)
+            }}>{loaderVisible ? 'Hide' : 'Show'}</button>
+            <div className="wrap">
+                <CSSTransition
+                    in={loaderVisible}  // триггер активации анимации
+                    timeOut={5000}
+                    classNames={{
+                        enterActive:'circle-show',
+                        enterDone:'circle-rotate',
+                        exitActive: 'circle-hide'
+                    }}
+                    mountOnEnter
+                    unmountOnExit
+                >
+                    <div className='circle'></div>
+                </CSSTransition>
+            </div>
+        </div>
+    );
+
 }
 
 export default App;
